81f68acde143313c5cee3a30afe8b8a6283bc978
aadv1k <aadv1k@outlook.com>
2023-08-01 14:38:40 +0530

    Setup frontend logic for file upload

--- js/index.js
+++ js/index.js
@@ -1,10 +1,40 @@
 import greetModule from "/wasm/greet.js";
 
-function checkIfHasWasmOrPanic() {
-    console.warn("[quickdoc] JS version of es6 or later is required for quickdoc to function");
-    console.warn("[quickdoc] WASM is used to process images entirely in the frontend");
+
+const MAX_FILES = 32; // TODO: this number is arbitiary
+
+
+if (typeof WebAssembly !== "object" && typeof WebAssembly.instantiate === "function") {
+    throw new Error("[quickdoc] WASM is required")
 }
 
-greetModule().then(instance => {
-    (instance.cwrap("greet"))();
-})
+const fileInput = document.getElementById("fileInput");
+
+function readFileAsBase64(file) {
+    console.assert(typeof file === "object", "readFileAsBase64 consumes a File object");
+    return new Promise((resolve, reject) => {
+        let fileReader = new FileReader();
+        fileReader.readAsDataURL(file);
+        fileReader.onload = () => {
+            resolve(fileReader.result);
+        };
+
+        fileReader.onerror = () => reject(fileReader.error);
+    })
+}
+
+fileInput.addEventListener("change", async (e) => {
+    if (e.target.files.length > MAX_FILES) {
+        throw new Error("[quickdoc] max amount of files exceeded");
+    }
+
+    const image = new Image();
+    image.src = await readFileAsBase64(e.target.files[0]);
+
+    let canvas = document.getElementById("primaryCanvas");
+    let context = canvas.getContext("2d");
+
+    canvas.width = image.width;
+    canvas.height = image.height;
+    context.drawImage(image, 0, 0);
+});
