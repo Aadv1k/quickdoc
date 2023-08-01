590da5b95ef3779cfb106a758542f4b21e0232a2
aadv1k <aadv1k@outlook.com>
2023-08-01 15:49:07 +0530

    Implement a decent file picker
    - has drag-n-drop
    - looks "standard" eg dashed box

--- js/index.js
+++ js/index.js
@@ -3,12 +3,50 @@ import greetModule from "/wasm/greet.js";
 
 const MAX_FILES = 32; // TODO: this number is arbitiary
 
+const selectedFiles = new Map();
+
+const fileBox = document.getElementById("fileBox");
+const fileInput = document.getElementById("fileInput");
+
+
+fileBox.addEventListener("click", () => fileInput.click())
+
+fileBox.addEventListener("drop", async (e) => {
+    e.preventDefault();
+    const files = e.dataTransfer.files;
+    if (files && files.length > 0) {
+        for (const file of files) {
+
+        if (selectedFiles.get(file.name)) continue;
+
+        const image = new Image();
+        image.src = await readFileAsBase64(file);
+
+        selectedFiles.set(file.name, image.src);
+
+        image.classList.add("img--thumb");
+        document.getElementById("imageContainer").appendChild(image);
+            
+        }
+    }
+    fileBox.classList.remove('file-box--dragover');
+})
+
+fileBox.addEventListener("dragleave", (e) => {
+    e.preventDefault();
+    fileBox.classList.remove('file-box--dragover');
+})
+
+fileBox.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    fileBox.classList.add('file-box--dragover');
+})
+
+
 
 if (typeof WebAssembly !== "object" && typeof WebAssembly.instantiate === "function") {
     throw new Error("[quickdoc] WASM is required")
 }
-
-const fileInput = document.getElementById("fileInput");
 
 function readFileAsBase64(file) {
     console.assert(typeof file === "object", "readFileAsBase64 consumes a File object");
@@ -18,23 +56,27 @@ function readFileAsBase64(file) {
         fileReader.onload = () => {
             resolve(fileReader.result);
         };
-
         fileReader.onerror = () => reject(fileReader.error);
     })
 }
 
+
+
 fileInput.addEventListener("change", async (e) => {
-    if (e.target.files.length > MAX_FILES) {
+    let files = e.target.files;
+    if (files.length > MAX_FILES) {
         throw new Error("[quickdoc] max amount of files exceeded");
     }
 
-    const image = new Image();
-    image.src = await readFileAsBase64(e.target.files[0]);
+    for (const file of files) {
+        if (selectedFiles.get(file.name)) continue;
 
-    let canvas = document.getElementById("primaryCanvas");
-    let context = canvas.getContext("2d");
+        const image = new Image();
+        image.src = await readFileAsBase64(file);
 
-    canvas.width = image.width;
-    canvas.height = image.height;
-    context.drawImage(image, 0, 0);
+        selectedFiles.set(file.name, image.src);
+
+        image.classList.add("img--thumb");
+        document.getElementById("imageContainer").appendChild(image);
+    }
 });
