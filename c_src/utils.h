e143c7e4fa186697ad09aef04a9d5c9765bf568a
aadv1k <aadv1k@outlook.com>
2023-08-02 21:28:19 +0530

    Add header guard

--- c_src/utils.h
+++ c_src/utils.h
@@ -1,3 +1,6 @@
+#ifdef CV_UTILS_H
+#define CV_UTILS_H
+
 #include <emscripten/emscripten.h>
 #include <stdint.h>
 #include <stddef.h>
@@ -8,3 +11,6 @@
 EMSCRIPTEN_KEEPALIVE void cv_squish_rgba_to_grayscale(uint8_t* data, size_t width, size_t height, uint8_t channels);
 
 EMSCRIPTEN_KEEPALIVE void cv_expand_grayscale_to_rgba(uint8_t* data, size_t width, size_t height, uint8_t channels);
+
+
+#endif
