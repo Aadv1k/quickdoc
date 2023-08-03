447e92d28fe38477061e1ec86ccd737401c725a9
aadv1k <aadv1k@outlook.com>
2023-08-03 11:27:26 +0530

    Update build script

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ set OUTPUT_DIR=wasm
 SET CC=emcc
 SET CCFLAGS=-O2 -s ASSERTIONS=1 -s ALLOW_MEMORY_GROWTH=1 -s EXPORTED_RUNTIME_METHODS="ccall,cwrap" -s EXPORTED_FUNCTIONS="['_malloc', '_free']"  -s EXPORT_ES6=1 -s MODULARIZE=1 
 
-SET SRC_FILES=c_src/utils.c
+SET SRC_FILES=c_src/utils.c c_src/crop_egdes.c
 
 if not exist %OUTPUT_DIR% mkdir %OUTPUT_DIR%
 
