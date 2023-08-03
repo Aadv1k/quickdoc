7bb6fc9e9f87b3488b2597aa59f8196a5096cc2f
aadv1k <aadv1k@outlook.com>
2023-08-03 17:00:50 +0530

    Delete redundant files, update build script

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ set OUTPUT_DIR=wasm
 SET CC=emcc
 SET CCFLAGS=-s ASSERTIONS=1 -s ALLOW_MEMORY_GROWTH=1 -s EXPORTED_RUNTIME_METHODS=["ccall","cwrap","getValue"] -s EXPORTED_FUNCTIONS="['_malloc', '_free']"  -s EXPORT_ES6=1 -s MODULARIZE=1 
 
-SET SRC_FILES=c_src/utils.c c_src/sobel.c c_src/image_parser.c c_src/crop.c
+SET SRC_FILES=c_src/utils.c c_src/sobel.c c_src/parse_image.c c_src/crop.c
 
 if not exist %OUTPUT_DIR% mkdir %OUTPUT_DIR%
 
