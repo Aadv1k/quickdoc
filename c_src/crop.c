8d5be049298a53ecd87108b03da155fcb84e1e64
aadv1k <aadv1k@outlook.com>
2023-08-04 13:34:17 +0530

    Implement y edge cropping

--- c_src/crop.c
+++ c_src/crop.c
@@ -53,19 +53,16 @@ size_t cv_get_right_edge(uint8_t* edgeData, size_t width, size_t height, uint8_t
 
 EMSCRIPTEN_KEEPALIVE
 size_t cv_crop_x_edge_grayscale_and_get_width(uint8_t* data, size_t width, size_t height, uint8_t channels, size_t leftEdge, size_t rightEdge) {
-    size_t croppedWidth = width - leftEdge - (width - rightEdge);
-    size_t newDataSize = sizeof(uint8_t) * croppedWidth * height * channels;
+    size_t croppedWidth = rightEdge - leftEdge;
+    size_t croppedHeight = height;
+    size_t newDataSize = sizeof(uint8_t) * croppedWidth * croppedHeight * channels;
     uint8_t* newData = (uint8_t*)malloc(newDataSize);
 
-    for (size_t i = 0; i < height; i++) {
-        for (size_t j = leftEdge; j <= rightEdge; j++) {
-            size_t currentIndex = (i * width * channels) + (j * channels);
-            size_t newIndex = (i * croppedWidth * channels) + ((j - leftEdge) * channels);
+    for (size_t i = 0; i < croppedHeight; i++) {
+        size_t currentIndex = i * width * channels + leftEdge * channels;
+        size_t newIndex = i * croppedWidth * channels;
 
-            for (size_t k = 0; k < channels; k++) {
-                newData[newIndex + k] = data[currentIndex + k];
-            }
-        }
+        memcpy(newData + newIndex, data + currentIndex, croppedWidth * channels);
     }
 
     memcpy(data, newData, newDataSize);
@@ -74,3 +71,21 @@ size_t cv_crop_x_edge_grayscale_and_get_width(uint8_t* data, size_t width, size_
     return croppedWidth;
 }
 
+EMSCRIPTEN_KEEPALIVE
+size_t cv_crop_y_edge_grayscale_and_get_height(uint8_t* data, size_t width, size_t height, uint8_t channels, size_t topEdge, size_t bottomEdge) {
+    size_t croppedHeight = bottomEdge - topEdge;
+    size_t croppedWidth = width * channels;
+    size_t newDataSize = sizeof(uint8_t) * croppedHeight * croppedWidth;
+    uint8_t* newData = (uint8_t*)malloc(newDataSize);
+
+    for (size_t i = topEdge; i < bottomEdge; i++) {
+        size_t currentIndex = i * width * channels;
+        size_t newIndex = (i - topEdge) * croppedWidth;
+        memcpy(newData + newIndex, data + currentIndex, croppedWidth);
+    }
+
+    memcpy(data, newData, newDataSize);
+    free(newData);
+
+    return croppedHeight;
+}
